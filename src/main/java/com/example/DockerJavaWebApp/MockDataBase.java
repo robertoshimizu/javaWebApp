f4ea611e2887353c1c8709b802f37854bf2b6183
robertoshimizu <rshimizubr@gmail.com>
2021-04-07 19:01:53 -0300

    added 2 new servlets

---
+++ src/main/java/com/example/DockerJavaWebApp/MockDataBase.java
@@ -0,0 +1,26 @@
+package com.example.DockerJavaWebApp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MockDataBase {
+
+    private static List<Consultant> lista = new ArrayList<>();
+
+    static {
+        Consultant consultant = new Consultant();
+        consultant.setName("Meryl Hawking");
+        Consultant consultant2 = new Consultant();
+        consultant2.setName("Jennifer Lawrence");
+        MockDataBase.lista.add(consultant);
+        MockDataBase.lista.add(consultant2);
+    }
+
+    public void adiciona(Consultant consultant) {
+        MockDataBase.lista.add(consultant);
+    }
+
+    public List<Consultant> getConsultants() {
+        return MockDataBase.lista;
+    }
+}
